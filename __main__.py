92adb2c238c4861b00e74a106eebe2ae4e8a45e0
David Mullen <dangermouse12885@gmail.com>
2024-01-08 08:33:19 -0500

    updating with various small changes

--- __main__.py
+++ __main__.py
@@ -85,7 +85,7 @@ def parse_feed(_nlu_url,_nlu_api_key,_classify_id,_financial_classify_id, _today
 		data = feedparser.parse(feed['feed_url'])
 		article_count = 0
 		for item in data.entries:
-			yesterday = datetime.now() - timedelta(days = 1)
+			yesterday = datetime.now() - timedelta(days = 3)
 			yesterday_utc = yesterday.replace(tzinfo = timezone.utc)
 			yesterday_utc_milli = int(yesterday_utc.timestamp() * 1000)
 			tomorrow = datetime.now() + timedelta(days = 1)
@@ -103,7 +103,7 @@ def parse_feed(_nlu_url,_nlu_api_key,_classify_id,_financial_classify_id, _today
 				continue
 				
 			article_count += 1
-			if article_count > 20:
+			if article_count > 100:
 				break
 				
 			if _use_sql:
@@ -155,7 +155,10 @@ def parse_feed(_nlu_url,_nlu_api_key,_classify_id,_financial_classify_id, _today
 				
 				class_map = {}
 				if "Dow Jones" in feed['publisher']:
-					class_map = classify_text(_nlu_url, _nlu_api_key, _financial_classify_id, article_title)
+					if financial_blacklist(article_title):
+						continue
+					else:
+						class_map = classify_text(_nlu_url, _nlu_api_key, _financial_classify_id, article_title)
 				else:
 					class_map = classify_text(_nlu_url, _nlu_api_key, _classify_id, article_title)
 				
@@ -191,21 +194,39 @@ def parse_feed(_nlu_url,_nlu_api_key,_classify_id,_financial_classify_id, _today
 							"negative_classifier": negative_classifier,
 							"lead_classifier": lead_classifier
 						}
-					}      
+					}
+					
+				if "Dow Jones" in feed['publisher']:
+					article_map[file_name]["metadata"]["article_text"] = item.content[0].value
+				
+				if "Engadget" in feed['feed_name']:
+					article_map[file_name]["metadata"]["article_text"] = item.description
 				
 	return {"article_map" : article_map }
+	
+	
+# @DEV: Return True if title contains blacklist terms, false otherwise
+
+def financial_blacklist(title):
+	blacklist_regexes = [r"crossword",r"pepper.*and\ssalt",r"firm\sretention\ssummary",r"corrections.*amplifications"]
+	for r in blacklist_regexes:
+		if re.search(r,title.lower()):
+			print("*** " + env + " SKIPPING ARTICLE USING FINANCIAL BLACKLIST: ", title)
+			return True
+	return False
 
 # @DEV: Filter the articles by their title. It should be passed a title and a Boolean to use a swear word filter or not. 
 # It will return True or False (False if it should be filtered out, True if not).
 def filter_by_title(title, swear_flag):
-	filtered_regexes = [r"\bshop",r"\bsale",r"\bphoto",r"\bprice","weed","marijuana","cannabis"]
-	for r in filtered_regexes:
-		if re.search(r,title):
-			print("*** " + env + " SKIPPING ARTICLE USING FILTER: ", title)
-			return False
+	# OLD HARD FILTER
+	#filtered_regexes = [r"\bshop",r"\bsale",r"\bphoto",r"\bprice","weed","marijuana","cannabis"]
+	#for r in filtered_regexes:
+	#	if re.search(r,title):
+	#		print("*** " + env + " SKIPPING ARTICLE USING FILTER: ", title)
+	#		return False
 
 	if swear_flag:
-		SWEAR_LIST = ["4r5e","5h1t","5hit","a55","anal","anus","ar5e","arrse","arse","ass","ass-fucker","asses","assfucker","assfukka","asshole","assholes","asswhole","a_s_s","b!tch","b00bs","b17ch","b1tch","ballbag","balls","ballsack","bastard","beastial","beastiality","bellend","bestial","bestiality","bi+ch","biatch","bitch","bitcher","bitchers","bitches","bitchin","bitching","bloody","blow job","blowjob","blowjobs","boiolas","bollock","bollok","boner","boob","boobs","booobs","boooobs","booooobs","booooooobs","breasts","buceta","bugger","bum","bunny fucker","butt","butthole","buttmuch","buttplug","c0ck","c0cksucker","carpet muncher","cawk","chink","cipa","cl1t","clit","clitoris","clits","cnut","cock","cock-sucker","cockface","cockhead","cockmunch","cockmuncher","cocks","cocksuck ","cocksucked ","cocksucker","cocksucking","cocksucks ","cocksuka","cocksukka","cok","cokmuncher","coksucka","coon","cox","crap","cum","cummer","cumming","cums","cumshot","cunilingus","cunillingus","cunnilingus","cunt","cuntlick ","cuntlicker ","cuntlicking ","cunts","cyalis","cyberfuc","cyberfuck ","cyberfucked ","cyberfucker","cyberfuckers","cyberfucking ","d1ck","damn","dick","dickhead","dildo","dildos","dink","dinks","dirsa","dlck","dog-fucker","doggin","dogging","donkeyribber","doosh","duche","dyke","ejaculate","ejaculated","ejaculates ","ejaculating ","ejaculatings","ejaculation","ejakulate","f u c k","f u c k e r","f4nny","fag","fagging","faggitt","faggot","faggs","fagot","fagots","fags","fanny","fannyflaps","fannyfucker","fanyy","fatass","fcuk","fcuker","fcuking","feck","fecker","felching","fellate","fellatio","fingerfuck ","fingerfucked ","fingerfucker ","fingerfuckers","fingerfucking ","fingerfucks ","fistfuck","fistfucked ","fistfucker ","fistfuckers ","fistfucking ","fistfuckings ","fistfucks ","flange","fook","fooker","fuck","fucka","fucked","fucker","fuckers","fuckhead","fuckheads","fuckin","fucking","fuckings","fuckingshitmotherfucker","fuckme ","fucks","fuckwhit","fuckwit","fudge packer","fudgepacker","fuk","fuker","fukker","fukkin","fuks","fukwhit","fukwit","fux","fux0r","f_u_c_k","gangbang","gangbanged ","gangbangs ","gaylord","gaysex","goatse","God","god-dam","god-damned","goddamn","goddamned","hardcoresex ","hell","heshe","hoar","hoare","hoer","homo","hore","horniest","horny","hotsex","jack-off ","jackoff","jap","jerk-off ","jism","jiz ","jizm ","jizz","kawk","knob","knobead","knobed","knobend","knobhead","knobjocky","knobjokey","kock","kondum","kondums","kum","kummer","kumming","kums","kunilingus","l3i+ch","l3itch","labia","lmfao","lust","lusting","m0f0","m0fo","m45terbate","ma5terb8","ma5terbate","masochist","master-bate","masterb8","masterbat*","masterbat3","masterbate","masterbation","masterbations","masturbate","mo-fo","mof0","mofo","mothafuck","mothafucka","mothafuckas","mothafuckaz","mothafucked ","mothafucker","mothafuckers","mothafuckin","mothafucking ","mothafuckings","mothafucks","mother fucker","motherfuck","motherfucked","motherfucker","motherfuckers","motherfuckin","motherfucking","motherfuckings","motherfuckka","motherfucks","muff","mutha","muthafecker","muthafuckker","muther","mutherfucker","n1gga","n1gger","nazi","nigg3r","nigg4h","nigga","niggah","niggas","niggaz","nigger","niggers ","nob","nob jokey","nobhead","nobjocky","nobjokey","numbnuts","nutsack","orgasim ","orgasims ","orgasm","orgasms ","p0rn","pawn","pecker","penis","penisfucker","phonesex","phuck","phuk","phuked","phuking","phukked","phukking","phuks","phuq","pigfucker","pimpis","piss","pissed","pisser","pissers","pisses ","pissflaps","pissin ","pissing","pissoff ","poop","porn","porno","pornography","pornos","prick","pricks","pron","pube","pusse","pussi","pussies","pussy","pussys ","rectum","retard","rimjaw","rimming","s hit","s.o.b.","sadist","schlong","screwing","scroat","scrote","scrotum","semen","sex","sh!+","sh!t","sh1t","shag","shagger","shaggin","shagging","shemale","shi+","shit","shitdick","shite","shited","shitey","shitfuck","shitfull","shithead","shiting","shitings","shits","shitted","shitter","shitters ","shitting","shittings","shitty ","skank","slut","sluts","smegma","smut","snatch","son-of-a-bitch","spac","spunk","s_h_i_t","t1tt1e5","t1tties","teets","teez","testical","testicle","tit","titfuck","tits","titt","tittie5","tittiefucker","titties","tittyfuck","tittywank","titwank","tosser","turd","tw4t","twat","twathead","twatty","twunt","twunter","v14gra","v1gra","vagina","viagra","vulva","w00se","wang","wank","wanker","wanky","whoar","whore","willies","willy","xrated","xxx"]
+		SWEAR_LIST = ["4r5e","5h1t","5hit","a55","ar5e","arrse","ass-fucker","assfucker","assfukka","asshole","assholes","asswhole","b!tch","b00bs","b17ch","b1tch","ballbag","ballsack","beastial","beastiality","bellend","bestial","bestiality","blow job","blowjob","blowjobs","boiolas","bollock","bollok","buceta","bunny fucker","butthole","buttmuch","c0ck","c0cksucker","carpet muncher","cawk","chink","cipa","cnut","cock-sucker","cockface","cockhead","cockmunch","cockmuncher","cocks","cocksuck","cocksucked","cocksucker","cocksucking","cocksucks","cocksuka","cocksukka","cok","cokmuncher","coksucka","coon","cox","crap","cum","cummer","cumming","cums","cumshot","cunilingus","cunillingus","cunnilingus","cunt","cuntlick","cuntlicke ","cuntlicking","cunts","cyalis","cyberfuc","cyberfuck","cyberfucked","cyberfucker","cyberfuckers","cyberfucking","d1ck","dickhead","dink","dinks","dirsa","dlck","dog-fucker","doggin","dogging","donkeyribber","doosh","duche","ejaculating","ejaculatings","ejakulate","f u c k","f u c k e r","f4nny","fag","fagging","faggitt","faggot","faggs","fagot","fagots","fags","fannyflaps","fannyfucker","fanyy","fcuk","fcuker","fcuking","feck","fecker","felching","fellate","fingerfuck","fingerfucked","fingerfucker","fingerfuckers","fingerfucking","fingerfucks","fistfuck","fistfucked","fistfucker","fistfuckers","fistfucking","fistfuckings","fistfucks","flange","fook","fooker","fuck","fucka","fucked","fucker","fuckers","fuckhead","fuckheads","fuckin","fucking","fuckings","fuckingshitmotherfucker","fuckme","fucks","fuckwhit","fuckwit","fudge packer","fudgepacker","fuk","fuker","fukker","fukkin","fuks","fukwhit","fukwit","fux","fux0r","f_u_c_k","gangbang","gangbanged","gangbangs","gaylord","gaysex","goatse","god-dam","god-damned","hardcoresex ","heshe","hoar","hoare","hoer","hore","horniest","hotsex","jack-off ","jackoff","jap","jerk-off","jism","jiz","jizm","jizz","kawk","knobead","knobed","knobend","knobhead","knobjocky","knobjokey","kock","kondum","kondums","kum","kummer","kumming","kums","kunilingus","l3i+ch","l3itch","labia","lusting","m0f0","m0fo","m45terbate","ma5terb8","ma5terbate","masochist","master-bate","masterb8","masterbat*","masterbat3","masterbate","masterbation","masterbations","mo-fo","mof0","mofo","mothafuck","mothafucka","mothafuckas","mothafuckaz","mothafucked ","mothafucker","mothafuckers","mothafuckin","mothafucking ","mothafuckings","mothafucks","mother fucker","motherfuck","motherfucked","motherfucker","motherfuckers","motherfuckin","motherfucking","motherfuckings","motherfuckka","motherfucks","muff","mutha","muthafecker","muthafuckker","muther","mutherfucker","n1gga","n1gger","nazi","nigg3r","nigg4h","nigga","niggah","niggas","niggaz","nigger","niggers","nob jokey","nobhead","nobjocky","nobjokey","numbnuts","nutsack","orgasim","orgasims","p0rn","pawn","pecker","penisfucker","phonesex","phuck","phuk","phuked","phuking","phukked","phukking","phuks","phuq","pigfucker","pimpis","pisser","pissers","pisses","pissflaps","pissin","pissing","pissoff","poop","porn","porno","pornography","pornos","pricks","pron","pube","pusse","pussi","pussies","pussys","rectum","retard","rimjaw","rimming","s hit","s.o.b.","sadist","schlong","screwing","scroat","scrote","scrotum","sh!+","sh!t","sh1t","shagger","shaggin","shagging","shemale","shi+","shit","shitdick","shite","shited","shitey","shitfuck","shitfull","shithead","shiting","shitings","shits","shitted","shitter","shitters","shitting","shittings","shitty","skank","slut","sluts","smegma","smut","son-of-a-bitch","spac","spunk","s_h_i_t","t1tt1e5","t1tties","teets","teez","testical","titfuck","titt","tittie5","tittiefucker","tittyfuck","tittywank","titwank","tosser","turd","tw4t","twat","twathead","twatty","twunt","twunter","v14gra","v1gra","vulva","w00se","wank","wanker","wanky","whoar","whore","willies","xrated","xxx"]
 		if any([element in title.split('-') for element in SWEAR_LIST]):
 			print("*** " + env + " SKIPPING ARTICLE USING SWEAR FILTER: ", title)
 			return False
