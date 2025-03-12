9886e6ee51d0736e98287e80481a975bb98a6e75
David Mullen <dangermouse12885@gmail.com>
2025-03-12 11:21:50 -0400

    updating parsing to skip NYT articles with empty firstparagraph field

--- __main__.py
+++ __main__.py
@@ -193,6 +193,15 @@ def parse_feed(_nlu_url,_nlu_api_key,_classify_id,_financial_classify_id, _today
 				
 				ct_start = time.perf_counter()
 				class_map = {}
+				
+				if "New York Times" in feed['publisher']:
+					if "ncr_firstparagraph" in item:
+						if len(item.ncr_firstparagraph) < 10:
+							print("*** " + env + " SKIPPING EMPTY NYT ARTICLE: ", article_title, " FEED:", feed['feed_url'])
+							continue
+					else:
+						print("*** " + env + " SKIPPING EMPTY NYT ARTICLE: ", article_title, " FEED:", feed['feed_url'])
+						continue
 				if "Dow Jones" in feed['publisher']:
 					if financial_blacklist(article_title):
 						continue
